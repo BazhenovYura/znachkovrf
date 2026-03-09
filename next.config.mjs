6da5084b798f395df15f951f61893cc818ef4a46
BazhenovYura <133841115+BazhenovYura@users.noreply.github.com>
2026-03-09 16:36:32 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // полезно оставить
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // обязательно для статического экспорта!
   },
+  output: 'export', // 👈 ТОЛЬКО ЭТО ДОБАВИТЬ
 }
 
 export default nextConfig
